ab7018067cbf631f1769c37651340f0f16491943
yohanG1106 <mjgallardo2@tip.edu.ph>
2026-04-17 09:50:51 +0800

    Add session debug test

---
+++ index/sessiontest.php
@@ -0,0 +1,8 @@
+﻿<?php
+include "db_connect.php";
+session_start();
+$_SESSION['test'] = 'hello';
+echo "Session ID: " . session_id() . "<br>";
+echo "Session data: " . print_r($_SESSION, true) . "<br>";
+echo "Save path: " . ini_get('session.save_path') . "<br>";
+echo "Save handler: " . ini_get('session.save_handler') . "<br>";
